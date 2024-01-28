3e0aec85781e414e1072665f26669bd164023250
Mihail93A <strannodan@mail.ru>
2024-01-29 01:17:18 +0500

    1

--- src/test/java/tests/ReqresInExtendedTests.java
+++ src/test/java/tests/ReqresInExtendedTests.java
@@ -1,3 +1,6 @@
+package tests;
+
+import models.LoginBodyModel;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
@@ -5,19 +8,37 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SelenoidTests {
+public class ReqresInExtendedTests {
 
     @Test
-    void checkTotal() {
+    void LoginWithBadPracticeTest() {
         String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+
+
+    @Test
+    void LoginWithModelTest() {
+        LoginBodyModel data = new LoginBodyModel();
+        data.setEmail("eve.holt@reqres.in");
+        data.setPassword("cityslicka");
 
         given()
                 .log().uri()
